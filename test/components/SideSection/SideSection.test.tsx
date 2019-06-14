79170d3dbf6403636fb3315865fb0bdd92e4ba58
Sushant Singh <sushantr134@gmail.com>
2019-06-14 21:07:42 +0530

    New Test Case added

--- test/components/SideSection/SideSection.test.tsx
+++ test/components/SideSection/SideSection.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import * as renderer from 'react-test-renderer';
 
 import SideNavMenu from '../../../app/components/SideSection/SideNavMenu/SideNavMenu';
@@ -7,36 +7,48 @@ import { MenuItem } from '../../../app/components/SideSection/SideNavItem/SideNa
 import { MemoryRouter } from 'react-router';
 
 describe('Testing of SideSection Component', () => {
+  let SideNavMenuTestComponent: any;
+
   const menuItems: Array<MenuItem> = [
     {
       text: 'Nav Item 1',
       id: 1,
       icon: '',
       to: ''
+    },
+    {
+      text: 'Nav Item 2',
+      id: 2,
+      icon: '',
+      to: '/launchpad'
     }
   ];
-  it('rendering SideNavItems correctly', () => {
-    const component = renderer
-      .create(
-        <MemoryRouter>
-          <SideNavMenu menuItems={menuItems} defaultSelected={1} />
-        </MemoryRouter>
-      )
-      .toJSON();
-    expect(component).toMatchSnapshot();
-  });
 
-  it('checking navItemName', () => {
-    const wrapper = mount(
+  beforeEach(() => {
+    SideNavMenuTestComponent = (
       <MemoryRouter>
         <SideNavMenu menuItems={menuItems} defaultSelected={1} />
       </MemoryRouter>
     );
+  });
+
+  it('rendering SideNavItems correctly', () => {
+    const component = renderer.create(SideNavMenuTestComponent).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('checking first navItemName', () => {
+    const wrapper = mount(SideNavMenuTestComponent);
     expect(
       wrapper
         .find('a')
         .first()
         .text()
     ).toBe('Nav Item 1');
+  });
+
+  it('Checking no. of List Items rendered based on props data', () => {
+    const wrapper = mount(SideNavMenuTestComponent);
+    expect(wrapper.find('.sideMenu').find('li').length).toBe(1); //test must be failed because we have 2 items in props above defined in var menuItems
   });
 });
